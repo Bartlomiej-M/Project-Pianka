e6adc732301a01d95e1225954abb1663ebc0532d
Bartek2 <bartek.machowski13@wp.pl>
2021-05-21 10:25:48 +0200

    Project-Myjnia-aktualizacja próbna

--- app/src/main/java/com/example/projekt_pianka_myjnia/AccountFragment.java
+++ app/src/main/java/com/example/projekt_pianka_myjnia/AccountFragment.java
@@ -48,11 +48,7 @@ import static android.content.ContentValues.TAG;
 
 import androidx.fragment.app.FragmentManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AccountFragment extends Fragment implements View.OnClickListener {
 
     private String userID;
@@ -63,46 +59,7 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
     FirebaseUser firebaseUser;
     DatabaseReference databaseReference;
     FirebaseAuth firebaseAuth;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AccountFragment() {
-
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
